913e716a2440bc4f901caecdb74b91f698981893
Chubs1 <h2ois4drinking@gmail.com>
2024-03-13 15:36:20 -0400

    Steps per Clue
    Created Steps per Clue plugin

--- src/test/java/com/StepsPerClue/PluginLauncher.java
+++ src/test/java/com/StepsPerClue/PluginLauncher.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.StepsPerClue;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginLauncher
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(StepsPerCluePlugin.class);
 		RuneLite.main(args);
 	}
 }
